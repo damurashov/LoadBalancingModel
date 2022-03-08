c7ad92c039927e9a8171826c944cfdb36de17600
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-08 10:27:49 +0300

    [:entity] Impl | __hash__(), __eq__(), add_task()

--- src/entity.py
+++ src/entity.py
@@ -3,18 +3,27 @@ from itertools import count
 
 @dataclass
 class Message:
-    ttl: int  # Time-to-live
+	ttl: int  # Time-to-live
 
-    # Scores
-    per: int  # How important a node's performance is
-    eff: int  # How important a node's efficiency is
-    dtr: int  # How important it is to distribute load evenly
-    crit: int  # How important it is to make decision now
+	# Scores
+	per: int  # How important a node's performance is
+	eff: int  # How important a node's efficiency is
+	dtr: int  # How important it is to distribute load evenly
+	crit: int  # How important it is to make decision now
 
 
 @dataclass
 class Node:
-    per: int  # Performance
-    eff: int  # Energy consumption (opposite to efficiency)
-    tasks: list  # List of tasks
-    identifier: int = field(default_factory=count().__next__)
+	per: int  # Performance
+	eff: int  # Energy consumption (opposite to efficiency)
+	tasks: list = field(default_factory=list)  # List of tasks
+	identifier: int = field(default_factory=count().__next__)
+
+	def __eq__(self, other):
+		return self.identifier == other.identifier
+
+	def __hash__(self):
+		return hash(self.identifier)
+
+	def add_task(self, task):
+		self.tasks.append(task)

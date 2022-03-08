d592f3dff9ac4fa6189d948482ede6e9612a05c4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-08 13:12:24 +0300

    [:cluster] Impl | The core - the balancing algorithm

---
+++ src/cluster.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
+
+from src import entity
+from src import ahp_solve
+
+
+def assign_message(cluster: nx.Graph, msg: entity.Message, decision_making_node: entity.Node):
+	"""
+	Bounces the message until its TTL goes to 0, or until it will be decided to assign the message to some node
+
+	:param cluster: Cluster of connected nodes
+	:param msg: Message encapsulating task info
+	:param decision_making_node: Master node
+	:return:
+	"""
+
+	if 0 == msg.ttl:
+		decision_making_node.add_task(msg)
+		return cluster
+
+	neighbors = tuple(cluster.neighbors(decision_making_node))
+	assigned_node, _ = ahp_solve.ahp_solve(msg, decision_making_node, *neighbors)
+
+	if assigned_node == decision_making_node:
+		decision_making_node.add_task(msg)
+		return cluster
+	else:
+		msg.ttl -= 1
+		return assign_message(cluster, msg, assigned_node)

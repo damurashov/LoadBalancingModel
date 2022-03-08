fbb2d228110d798a075364147259f8d2fd09d4a4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-08 12:20:06 +0300

    [test:*data] Impl

---
+++ test/test_data.py
@@ -0,0 +1,30 @@
+import unittest
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
+from src import entity
+from src import data
+from src.generic import Logging
+
+
+class TestData(unittest.TestCase):
+
+	def test_generate_message(self):
+		m = data.generate_message(data.RandomGeneration())
+		Logging.debug(TestData.test_generate_message, m)
+
+		m_per_prev = m.per
+		m.per = None
+		m2 = data.generate_message(data.RandomGeneration(), m)
+		max_iter_threshold = 100
+
+		while m2.per == m_per_prev and threshold:
+			m2 = data.generate_message(data.RandomGeneration(), m)
+			max_iter_threshold -= 1
+
+		Logging.debug(TestData.test_generate_message, m2)
+		self.assertTrue(m2.per != m_per_prev)
+
+	def test_generate_node(self):
+		m = data.generate_node(data.RandomGeneration())
